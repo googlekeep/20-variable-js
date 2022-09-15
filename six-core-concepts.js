9632c401e13a587e9d5eafd40a16d961380503f5
googlekeep <info.workingforyou@gmail.com>
2022-09-16 00:11:15 +0530

    javascript concepts and apply revision with four challenges

---
+++ six-core-concepts.js
@@ -0,0 +1,24 @@
+// 1 variable (declare, naming convention, variable Types, math operastion)
+// 2 conditional (comparison: >,<, >=, <=, ===,, !==, if-else if-else)
+// 3 array (declare, index, get values, set values, indexof, push, pop)
+// 4 loop (for loop, while loop, break, continue)
+// 5 function (declare, parameters, retun, call use the returned value form a function)
+// 6 object (declare, properties, keys,values, get prop value, set value, loop object)
+
+
+
+
+// 1 google: function declare vs function expression
+// 2 google: arrow function
+// 3 loop vs function
+// 4 array vs Object
+// 5 integer vs Float
+// 6 == vs ==
+// 7 differences between: var, let, const
+
+
+// 1 celsius ----> fahrenheit
+// 2 fahrenheiht ----> celsius
+// 3 simple interest : principal, interset rate , time ---> interest amount
+// 4 complex interset:
+
