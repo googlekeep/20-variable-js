55e34714a6e255f220fdaa5aafc573f22c1c27dd
googlekeep <info.workingforyou@gmail.com>
2022-09-15 21:26:35 +0530

    20-1 js conceps recap, var,let,and const

---
+++ recap.js
@@ -0,0 +1,56 @@
+// variable
+var myName = 'khan';
+
+// math operation : +, -,*, /
+// shorthand : +=, --, /=
+// ++, --
+
+// array
+var friens = ['abul','babul','cabul', 'ebul'];
+var thirdFriend = friends[2];
+friens[3];
+
+
+// conditionals
+// >, <, >=, <=, ===, ==, !=, !==
+
+if (friens.length<2){
+  console.log('fokor tor kono friend nai')
+}
+  else{
+
+  }
+
+
+
+// loop
+
+var number = 0;
+while(number <=6){
+    console.log(number);
+    number++;
+
+}
+
+for(var i = 0, i <= 10; i++){
+
+}
+  
+
+
+// function
+function isMoonUp(time){
+    if(time>7){
+        return true;
+    }
+}
+
+
+// object
+
+var jantus = {
+    height :60,
+    romanticism:'hevvy',
+    bappErtaka:'borolox'
+
+}
---
+++ var-let-const.js
@@ -0,0 +1,5 @@
+let age = 11;
+age = 12;
+
+const countryName = 'bangladesg';
+console.log(age, countryName);
