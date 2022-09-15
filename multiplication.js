187c1d057b28615f80d9b462eb7f5a12386f65ba
googlekeep <info.workingforyou@gmail.com>
2022-09-15 23:28:38 +0530

    20-7 calculate factorial of a number using for loop

---
+++ multiplication.js
@@ -0,0 +1,12 @@
+// 1 * 2 * 3 * 4  * 5 * 7
+
+function multiplicationOfNumber (number){
+    let result = 1;
+    for (let i  = 1; i <= 7; i++){
+        result = result * i;
+    }
+    return result;
+}
+
+const result = multiplicationOfNumber(7);
+console.log(result);
