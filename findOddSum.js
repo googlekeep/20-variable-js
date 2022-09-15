3b48e373d0894908ccff6ad3964e10a3a145b1fe
googlekeep <info.workingforyou@gmail.com>
2022-09-15 23:12:18 +0530

    20.6 get odd Numbers of an arreay and get sum of an array

--- findOddSum.js
+++ findOddSum.js
@@ -4,8 +4,26 @@ function getSumAnArray (numbers){
         const index = i;
         const element = numbers[index]
         sum = sum + element;
-        console.log(index, element, sum);
+        // console.log(index, element, sum);
     }
 }
+
+function getOddNumberAnArray(numbers){
+    const oddNumbers = [];
+    for(let i = 0; i < numbers.length; i++){
+        const index = i;
+        const element = numbers[index];
+        if(element % 2 !== 1){
+            console.log(index,element);
+            oddNumbers.push(element);
+        }
+        
+ 
+    }
+
+}
+
+
 const myNumbers = [12,65,45,78,32,45,91];
+   oddNumbers  =  getOddNumberAnArray(myNumbers);
 getSumAnArray(myNumbers);
